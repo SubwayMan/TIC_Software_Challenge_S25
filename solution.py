6dbf49833031d0c4e0cdb57681760c6becdc4751
Kayla <kayla76815@gmail.com>
2025-05-24 11:48:05 -0400

    updated challenge 1

--- solution.py
+++ solution.py
@@ -40,12 +40,16 @@ try:
     if challengeLevel == 1:
         control.start_keyboard_input()
         while rclpy.ok():
+            rclpy.spin_once(robot, timeout_sec=0.1)
+            time.sleep(0.1)
             scan = lidar.checkScan()
-            if ( 0 <= lidar.detect_obstacle_in_cone(scan, 0.15, 0, 20)[0] <= 0.1):
-                print("STOP")
+            dist = lidar.detect_obstacle_in_cone(scan, 0.50, 0, 20)
+            print(dist)
+            if ( 0.0 <= dist[0] <= 0.2):
+                #print("STOP")
                 control.stop_keyboard_input()
-                control.move_backward()
-                print("START")
+                control.set_cmd_vel(-0.5,0.0,0.5)
+                #print("START")
                 control.start_keyboard_input()
             # Write your solution here for challenge level 1
             # It is recommended you use functions for aspects of the challenge that will be resused in later challenges
